6d8535addcf8918905cb668d7f8a43b9108237d1
Reem Sakr <reemsakr060@gmail.com>
2024-05-11 15:41:59 +0300

    test user repository and event controller

---
+++ src/test/java/gov/iti/spring/testing/persistence/UserRepositoryTest.java
@@ -0,0 +1,40 @@
+package gov.iti.spring.testing.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@Testcontainers
+class UserRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:12.3")
+            .withDatabaseName("test")
+            .withUsername("user")
+            .withPassword("password");
+
+
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void test(){
+        assertEquals(userRepository.findAll().size(),0);
+    }
+}
---
+++ src/test/java/gov/iti/spring/testing/web/controllers/EventControllerTest.java
@@ -0,0 +1,54 @@
+package gov.iti.spring.testing.web.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.iti.spring.testing.config.WebSecurityConfig;
+import gov.iti.spring.testing.domain.Event;
+import gov.iti.spring.testing.service.EventService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(EventController.class)
+@Import(WebSecurityConfig.class)
+class EventControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private EventController eventController;
+
+    @MockBean
+    private EventService eventService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    void getAllEvents()throws Exception {
+        when(eventService.getAll()).thenReturn(List.of(new Event(), new Event()));
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        List<Event> events = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>(){});
+
+        Assertions.assertThat(events.size()).isEqualTo(2);
+    }
+}
